9d00d3db58982728fdf5e7d308c8c4ab0ba1baec
Kent Larsson <kent.larsson@gmail.com>
2022-02-08 21:17:00 +0100

    Made the Polybar config a Chezmoi template.

--- run_scripted_configuration.sh
+++ run_scripted_configuration.sh
@@ -31,4 +31,16 @@ if [[ $(chezmoi data | jq -r '.chezmoi.os') == 'linux' ]]; then
   fi
 
   set_prop 'hw' 'gfxdriver' "$(lspci -k | grep -A 2 -E '(VGA|3D)' | grep driver | awk '{ print $5 }')"
+
+  if has getent && has ip; then
+    host_ip=$(getent ahosts 'google.com' | awk '{ print $1; exit }') # get first ip in case of multiple
+    network_device="$(ip route get "$host_ip" | grep -Po '(?<=(dev ))(\S+)')"
+    if [[ -d "/sys/class/net/$network_device/wireless" ]]; then
+      set_prop 'network' 'wireless' "$network_device"
+    else
+      set_prop 'network' 'wired' "$network_device"
+    fi
+  else
+    warn 'missing getent or ip, cannot detect active network interface'
+  fi
 fi
