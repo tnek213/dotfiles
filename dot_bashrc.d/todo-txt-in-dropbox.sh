8c912d2d6b0ee97dc27bfcf78f5cc2556b63c23e
Kent Larsson <kent.larsson@gmail.com>
2022-02-10 23:05:11 +0100

    Fix script.

--- dot_bashrc.d/todo-txt-in-dropbox.sh
+++ dot_bashrc.d/todo-txt-in-dropbox.sh
@@ -5,7 +5,7 @@ if [[ -d ~/Dropbox ]]; then
 
   if command -v todo &>/dev/null; then
     alias t=todo
-  elif command -v todo.sh &>/dev/null
+  elif command -v todo.sh &>/dev/null; then
     alias t=todo.sh
   fi
 fi
